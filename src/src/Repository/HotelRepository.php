1c35a4334bd4fa12ab2293e2447e5dc02f066f4c
Erfan Shamabadi <e.shamabadi@gmail.com>
2022-06-09 19:46:14 +0200

    assignemt 4

--- src/src/Repository/HotelRepository.php
+++ src/src/Repository/HotelRepository.php
@@ -10,16 +10,14 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Hotel>
- *
  * @method Hotel|null find($id, $lockMode = null, $lockVersion = null)
  * @method Hotel|null findOneBy(array $criteria, array $orderBy = null)
  * @method Hotel[]    findAll()
  * @method Hotel[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class HotelRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class HotelRepository extends ServiceEntityRepository {
+
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Hotel::class);
     }
 
@@ -27,8 +25,7 @@ class HotelRepository extends ServiceEntityRepository
      * @throws ORMException
      * @throws OptimisticLockException
      */
-    public function add(Hotel $entity, bool $flush = true): void
-    {
+    public function add(Hotel $entity, bool $flush = true): void {
         $this->_em->persist($entity);
         if ($flush) {
             $this->_em->flush();
@@ -39,40 +36,24 @@ class HotelRepository extends ServiceEntityRepository
      * @throws ORMException
      * @throws OptimisticLockException
      */
-    public function remove(Hotel $entity, bool $flush = true): void
-    {
+    public function remove(Hotel $entity, bool $flush = true): void {
         $this->_em->remove($entity);
         if ($flush) {
             $this->_em->flush();
         }
     }
 
-    // /**
-    //  * @return Hotel[] Returns an array of Hotel objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('h')
-            ->andWhere('h.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('h.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+    public function searchByName($hotelNamePart) {
 
-    /*
-    public function findOneBySomeField($value): ?Hotel
-    {
-        return $this->createQueryBuilder('h')
-            ->andWhere('h.exampleField = :val')
-            ->setParameter('val', $value)
+        $qb = $this->createQueryBuilder('h');
+
+        return $qb
+            ->andWhere(
+                $qb->expr()->like('h.name', ':name')
+            )
+            ->setParameter('name', "%$hotelNamePart%")
+            ->orderBy('h.name', 'ASC')
             ->getQuery()
-            ->getOneOrNullResult()
-        ;
+            ->getResult();
     }
-    */
 }
